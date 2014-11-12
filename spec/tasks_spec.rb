753d478b7762c044a558bbfbd5a1fe156721af43
M1chael <asoi@mail.ru>
2014-11-12 16:23:43 +0500

    spec for Tasks#last updated with only 10 tasks showing

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -4,8 +4,19 @@ require 'tasks'
 describe Tasks do
   timestamp = Time.now.to_i
   let(:tasks) { [
-        { id: 1, title: 'Тестовая задача 1', description: 'Описание тестовой задачи 1', created_at: timestamp },
-        { id: 2, title: 'Тестовая задача 2', description: 'Описание тестовой задачи 2', created_at: timestamp }
+        { id: 1, title: 'Тестовая задача 1', description: 'Описание тестовой задачи 1', created_at: 1415791040 },
+        { id: 2, title: 'Тестовая задача 2', description: 'Описание тестовой задачи 2', created_at: 1415791041 },
+        { id: 3, title: 'Тестовая задача 3', description: 'Описание тестовой задачи 3', created_at: 1415791042 },
+        { id: 4, title: 'Тестовая задача 4', description: 'Описание тестовой задачи 4', created_at: 1415791043 },
+        { id: 5, title: 'Тестовая задача 5', description: 'Описание тестовой задачи 5', created_at: 1415791044 },
+        { id: 6, title: 'Тестовая задача 6', description: 'Описание тестовой задачи 6', created_at: 1415791045 },
+        { id: 7, title: 'Тестовая задача 7', description: 'Описание тестовой задачи 7', created_at: 1415791046 },
+        { id: 8, title: 'Тестовая задача 8', description: 'Описание тестовой задачи 8', created_at: 1415791047 },
+        { id: 9, title: 'Тестовая задача 9', description: 'Описание тестовой задачи 9', created_at: 1415791048 },
+        { id: 10, title: 'Тестовая задача 10', description: 'Описание тестовой задачи 10', created_at: 1415791049 },
+        { id: 11, title: 'Тестовая задача 11', description: 'Описание тестовой задачи 11', created_at: 1415791050 },
+        { id: 12, title: 'Тестовая задача 12', description: 'Описание тестовой задачи 12', created_at: 1415791051 },
+        { id: 13, title: 'Тестовая задача 13', description: 'Описание тестовой задачи 13', created_at: 141579104052 },
       ] }
   describe 'Tasks#create' do
     it 'should save task to DB' do
@@ -30,7 +41,7 @@ describe Tasks do
         db_tasks.insert(title: task[:title], description: task[:description], created_at: task[:created_at])
       end
       test_tasks = Tasks.new(db)
-      expect(test_tasks.last).to eq(tasks.sort{ |x, y| y[:id] <=> x[:id] })
+      expect(test_tasks.last).to eq(tasks.sort{ |x, y| y[:id] <=> x[:id] }.first(10))
       FileUtils.cp(CLEANDB, TESTDB)
     end
   end

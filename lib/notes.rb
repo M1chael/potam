104a453e15000245c89c61766c88d2572f8e923f
M1chael <asoi@mail.ru>
2014-11-19 20:39:00 +0500

    tasks, subtasks and notes refactored

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,17 +1,3 @@
-class Notes
-
-  attr_reader :new_note_id
-
-  def initialize(db)
-    @db = db
-    @notes = @db[:notes]
-  end
-
-  def create(options = {})
-    options.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-    @new_note_id = @notes.insert(task_id: @task_id, text: @text, created_at: @created_at)
-  end
+class Notes < DB
 
 end
--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,29 +1,3 @@
-class Tasks
-
-  attr_reader :new_task_id
-
-  def initialize(db)
-    @db = db
-    @tasks = @db[:tasks]
-  end
-
-  def create(options = {})
-    options.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-    @new_task_id = @tasks.insert(title: @title, description: @description, created_at: @created_at)
-  end
-
-  def last
-    @tasks.order(Sequel.desc(:id)).limit(10).all
-  end
-
-  def list
-    @tasks.order(Sequel.desc(:id)).all
-  end
-
-  def info(id)
-    @tasks.where("id = ?", id.to_i).first
-  end
+class Tasks < DB
 
 end

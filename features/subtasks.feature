d7bb24e6ca25240ecc64493accf18644ab8611bc
M1chael <asoi@mail.ru>
2014-11-20 10:23:43 +0500

    scenario of marking subtask finished added to subtasks feature

--- features/subtasks.feature
+++ features/subtasks.feature
@@ -1,7 +1,7 @@
 Feature: Adding subtask
 	As a time-manager
   In order to plan my activity
-  I want to add a subtask
+  I want to manage subtasks
   
   Background:
     Given following tasks exists:
@@ -9,9 +9,13 @@ Feature: Adding subtask
       | Тестовая задача 1   | Описание     | 1415812561 |
       | Тестовая задача 2   | Описание     | 1415812566 |
 
-  Scenario: Successfully add tasks
+  Scenario: Successfully add subtasks
     When I run command `-t 2 add`
     And I type a subtask title "Подзадача 1"
     Then I should see "Подзадача #1 к задаче #2 добавлена"
+
+  Scenario: Successfully mark subtask finished
+    When I run command `-s 1`
+    Then I should see "Подзадача 'Подзадача 1' завершена"
 
   #  Scenario: Add multiline descriptions for tasks
--- spec/subtasks_spec.rb
+++ spec/subtasks_spec.rb
@@ -2,33 +2,41 @@ require 'spec_helper'
 require 'subtasks'
 
 describe Subtasks do
-  let(:subtasks) { [
-      { id: 1, task_id: 1, title: 'Подзадача 1', status: 0, created_at: 1415791040 },
-      { id: 2, task_id: 2, title: 'Подзадача 2', status: 0, created_at: 1415791041 },
-      { id: 3, task_id: 3, title: 'Подзадача 3', status: 0, created_at: 1415791042 },
-      { id: 4, task_id: 4, title: 'Подзадача 4', status: 0, created_at: 1415791043 },
-      { id: 5, task_id: 5, title: 'Подзадача 5', status: 0, created_at: 1415791044 },
-      { id: 6, task_id: 6, title: 'Подзадача 6', status: 0, created_at: 1415791045 },
-      { id: 7, task_id: 7, title: 'Подзадача 7', status: 0, created_at: 1415791046 },
-      { id: 8, task_id: 8, title: 'Подзадача 8', status: 0, created_at: 1415791047 },
-      { id: 9, task_id: 9, title: 'Подзадача 9', status: 0, created_at: 1415791048 },
-      { id: 10, task_id: 1, title: 'Подзадача 10', status: 0, created_at: 1415791049 },
-      { id: 11, task_id: 1, title: 'Подзадача 11', status: 0, created_at: 1415791050 },
-      { id: 12, task_id: 1, title: 'Подзадача 12', status: 0, created_at: 1415791051 },
-      { id: 13, task_id: 1, title: 'Подзадача 13', status: 0, created_at: 141579104052 },
-    ] }
-
-  describe 'Subtasks#create' do
-    it 'should save subtask to DB' do
-      db = double(Sequel.sqlite("#{File.expand_path(File.dirname(__FILE__))}/../db/potam.db"))
-      db_subtasks = double
-      subtasks.each do |subtask|
-        allow(db).to receive(:[]).with(:subtasks) { db_subtasks }
-        expect(db_subtasks).to receive(:insert).with(
-          task_id: subtask[:task_id], title: subtask[:title], status: 0, created_at: subtask[:created_at])
-        test_subtasks = Subtasks.new(db)
-        test_subtasks.create(task_id: subtask[:task_id], title: subtask[:title], status: 0, created_at: subtask[:created_at])
-      end
+  describe 'Subtasks#finish!' do
+    it 'should change status to finished' do
+      
     end
   end
 end
+
+# describe Subtasks do
+#   let(:subtasks) { [
+#       { id: 1, task_id: 1, title: 'Подзадача 1', status: 0, created_at: 1415791040 },
+#       { id: 2, task_id: 2, title: 'Подзадача 2', status: 0, created_at: 1415791041 },
+#       { id: 3, task_id: 3, title: 'Подзадача 3', status: 0, created_at: 1415791042 },
+#       { id: 4, task_id: 4, title: 'Подзадача 4', status: 0, created_at: 1415791043 },
+#       { id: 5, task_id: 5, title: 'Подзадача 5', status: 0, created_at: 1415791044 },
+#       { id: 6, task_id: 6, title: 'Подзадача 6', status: 0, created_at: 1415791045 },
+#       { id: 7, task_id: 7, title: 'Подзадача 7', status: 0, created_at: 1415791046 },
+#       { id: 8, task_id: 8, title: 'Подзадача 8', status: 0, created_at: 1415791047 },
+#       { id: 9, task_id: 9, title: 'Подзадача 9', status: 0, created_at: 1415791048 },
+#       { id: 10, task_id: 1, title: 'Подзадача 10', status: 0, created_at: 1415791049 },
+#       { id: 11, task_id: 1, title: 'Подзадача 11', status: 0, created_at: 1415791050 },
+#       { id: 12, task_id: 1, title: 'Подзадача 12', status: 0, created_at: 1415791051 },
+#       { id: 13, task_id: 1, title: 'Подзадача 13', status: 0, created_at: 141579104052 },
+#     ] }
+
+#   describe 'Subtasks#create' do
+#     it 'should save subtask to DB' do
+#       db = double(Sequel.sqlite("#{File.expand_path(File.dirname(__FILE__))}/../db/potam.db"))
+#       db_subtasks = double
+#       subtasks.each do |subtask|
+#         allow(db).to receive(:[]).with(:subtasks) { db_subtasks }
+#         expect(db_subtasks).to receive(:insert).with(
+#           task_id: subtask[:task_id], title: subtask[:title], status: 0, created_at: subtask[:created_at])
+#         test_subtasks = Subtasks.new(db)
+#         test_subtasks.create(task_id: subtask[:task_id], title: subtask[:title], status: 0, created_at: subtask[:created_at])
+#       end
+#     end
+#   end
+# end

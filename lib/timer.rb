cb9c05a85a90785e9149344352f68a4574abfa83
M1chael <asoi@mail.ru>
2014-11-28 21:46:00 +0500

    timer#last_mon spec passed

--- lib/timer.rb
+++ lib/timer.rb
@@ -4,4 +4,9 @@ module Timer
     time = ENV['POTAM'] == 'test' ? ENV['POTAMTIME'].to_i : Time.now.to_i
   end
 
+  def last_mon
+    mon = Time.at(now - (Time.at(now).wday - 1) * 24 * 60 * 60)
+    Time.new(mon.year, mon.month, mon.day).to_i
+  end
+
 end
--- spec/timer_spec.rb
+++ spec/timer_spec.rb
@@ -6,19 +6,26 @@ end
 
 describe Timer do
   let(:test) { Test.new }
-  let(:time) { 1415942161 }
+  let(:now) { 1415942161 }
+  let(:last_mon) { 1415559600 }
+
+  around(:example) do |example|
+    ENV['POTAMTIME'] = now.to_s
+    ENV['POTAM'] = 'test'
+    example.run
+    ENV['POTAM'] = 'production'
+  end
 
   describe '#now' do
-
-    around(:example) do |example|
-      ENV['POTAMTIME'] = time.to_s
-      ENV['POTAM'] = 'test'
-      example.run
-      ENV['POTAM'] = 'production'
-    end
-
     it 'should return test timestamp' do
-      expect(test.now).to eq(time)
+      expect(test.now).to eq(now)
     end
   end
+
+  describe '#last_mon' do
+    it 'should return last mondau 00:00:00 timestamp' do
+      expect(test.last_mon).to eq(last_mon)
+    end
+  end
+
 end
